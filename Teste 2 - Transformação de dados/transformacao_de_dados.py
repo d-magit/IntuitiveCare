14f30e19c637dc7bcba8ff08a6a214b4f50e2826
Davi <48662592+d-magit@users.noreply.github.com>
2022-01-06 23:25:30 -0300

    Início de extração - primeira tabela
    Conteúdo adicionado:
    Código para extração da primeira tabela e montagem de seu CSV.

--- Teste 2 - Transformação de dados/transformacao_de_dados.py
+++ Teste 2 - Transformação de dados/transformacao_de_dados.py
@@ -1,6 +1,27 @@
 from tabula import read_pdf
+import os
 options = ["-Dorg.slf4j.simpleLogger.defaultLogLevel=off", "-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog"]
 filename = 'padrao-tiss_componente-organizacional_202111.pdf' ## PDF file name
 
-# Reading PDF
+## Reading PDF
 df = read_pdf(filename, pages=[114, 115, 116, 117, 118, 119, 120], encoding='utf-8', java_options=options)
+##
+
+## Extracting tables and creating CSVs
+# First table
+first_table_name = ''
+final = ''
+df[0].to_csv('30.csv', encoding='utf-8')
+with open(f'30.csv', 'r', encoding='utf-8') as f:
+    file = f.read().split('\n')
+    first_table_name = file[0][1:]
+    for i in file[1:-1]:
+        content = i.split(',')[1]
+        first_space = content.find(' ')
+        first_entry = content[:first_space]
+        second_entry = content[first_space + 1:]
+        final += first_entry + ',' + second_entry + '\n'
+os.remove('30.csv')
+with open(first_table_name, 'w', encoding='utf-8') as f:
+    f.write(final[:-1])
+##
